013098651f465f580f2540a4aeedc7773b9b720e
khaja baba <shaik.khajababa.00@gmail.com>
2024-12-09 00:19:08 +0530

    Update real_button_press_location.ino

--- real_button_press_location.ino
+++ real_button_press_location.ino
@@ -1,56 +1,43 @@
 #include <TinyGPS++.h>
-#include <HardwareSerial.h>
-#include <WiFi.h>
-#include <BlynkSimpleEsp32.h>
+#include <BlynkSimpleSIM800.h>
+#include <SoftwareSerial.h>
 
-// Blynk credentials
-char auth[] = "YourBlynkAuthToken";  
-char ssid[] = "YourWiFiSSID";        
-char pass[] = "YourWiFiPassword";    
+#define BUTTON_PIN 0
+#define GPS_RX_PIN 16
+#define GPS_TX_PIN 17
 
 TinyGPSPlus gps;
-HardwareSerial gpsSerial(1); 
+HardwareSerial mySerial(1);
 
-const int buttonPin = 25;  
+char auth[] = "YOUR_BLYNK_AUTH_TOKEN";
 
 void setup() {
-
   Serial.begin(115200);
-  
-  
-  gpsSerial.begin(9600, SERIAL_8N1, 16, 17); // RX=16, TX=17
-  
-
-  WiFi.begin(ssid, pass);
-  Blynk.begin(auth, ssid, pass);
-  
-  // Configure button pin as input with pull-up resistor
-  pinMode(buttonPin, INPUT_PULLUP);
+  mySerial.begin(9600, SERIAL_8N1, 4, 5);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  Serial1.begin(9600, SERIAL_8N1, GPS_RX_PIN, GPS_TX_PIN);
+  Blynk.begin(auth, mySerial, "APN_NAME", "APN_USERNAME", "APN_PASSWORD");
 }
 
 void loop() {
   Blynk.run();
-  
-  while (gpsSerial.available() > 0) {
-    gps.encode(gpsSerial.read());
-  }
-  
-  if (digitalRead(buttonPin) == LOW) {
-    sendLocationToBlynk();
-    delay(1000); // debounce delay
-  }
-}
 
-void sendLocationToBlynk() {
-  if (gps.location.isValid()) {
-    double latitude = gps.location.lat();
-    double longitude = gps.location.lng();
-    String location = "Lat: " + String(latitude, 6) + ", Lng: " + String(longitude, 6);
+  if (digitalRead(BUTTON_PIN) == LOW) {
+    while (Serial1.available() > 0) {
+      gps.encode(Serial1.read());
+    }
+
+    if (gps.location.isUpdated()) {
+      double latitude = gps.location.lat();
+      double longitude = gps.location.lng();
+      Serial.print("Latitude: ");
+      Serial.print(latitude, 6);
+      Serial.print(" Longitude: ");
+      Serial.println(longitude, 6);
+      Blynk.virtualWrite(V1, latitude);
+      Blynk.virtualWrite(V2, longitude);
+    }
     
-    Serial.println(location);
-    
-    Blynk.virtualWrite(V1, location); 
-  } else {
-    Serial.println("Waiting for GPS signal...");
+    delay(2000);
   }
 }
